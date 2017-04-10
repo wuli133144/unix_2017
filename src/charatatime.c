eb58776486b701f3de286a8049126d3ddd77531a
wuli133144 <183358846@qq.com>
2017-04-10 22:39:55 +0800

    buffer error

---
+++ src/charatatime.c
@@ -0,0 +1,46 @@
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "common.h"
+
+
+
+static void printx(char *);
+
+//some error happened 
+//buffer errror
+int doit(int argc,char **argv){
+
+   pid_t pid;
+   pid=Fork();
+    switch (pid)
+    {
+        case 0:{//child proc
+            printx("output from child\n");
+            break;
+        }
+        default:
+            printx("output from parent\n");
+            break;
+    }
+    exit(0);
+
+}
+
+
+static void printx(char *str){
+    char *ptr;
+    char c;
+    setbuf(stdout,NULL);//Set no buffer
+
+    for(ptr=str;(c=*ptr++)!=0;)
+    {
+        putc(c,stdout);
+    }
+
+}
+
+int main(int argc,char **argv){
+    return doit(argc,argv);
+}
